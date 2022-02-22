fec4977f0932e77d9c7e13844eff9daf912a5a8e
pavaoana <anapavao2@gmail.com>
2022-02-22 22:38:17 +0000

    iterations 1,2 and 5 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import contactsArr from "./contacts.json";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  let fiveContacts = contactsArr.slice(0, 5);
+  const [allContacts, setAllContacts] = useState(fiveContacts);
+
+  const deleteContact = (contactId) => {
+    const filteredContacts = allContacts.filter((contact) => {
+      return contact.id !== contactId;
+    });
+    setAllContacts(filteredContacts);
+  };
+
+  // const addRandomContact = () => {
+  //   let extraContacts = contactsArr.slice(5);
+  //   let randomContact =
+  //     extraContacts[Math.floor(Math.random() * extraContacts.length)];
+  // };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Iron Contacts</h1>
+      {/* <button onClick={addRandomContact}>Add Random Contact</button> */}
+      {/* <button onClick={sortByPopularity}> Sort By Popularity</button> */}
+      {/* <button onClick={sortByName}> Sort By Name</button> */}
+      <table>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
+          <th>Actions</th>
+        </tr>
+
+        {allContacts.map((contact) => (
+          <tr key={contact.id}>
+            <th>
+              <img src={contact.pictureUrl} alt={contact.name}></img>
+            </th>
+            <th>{contact.name}</th>
+            <th>{contact.popularity.toFixed(2)}</th>
+            <th>
+              {contact.wonOscar ? (
+                <img
+                  src="https://a.slack-edge.com/production-standard-emoji-assets/13.0/google-medium/1f3c6@2x.png"
+                  alt="trophy"
+                />
+              ) : (
+                ""
+              )}
+            </th>
+            <th>
+              {contact.wonEmmy ? (
+                <img
+                  src="https://a.slack-edge.com/production-standard-emoji-assets/13.0/google-medium/1f3c6@2x.png"
+                  alt="trophy"
+                />
+              ) : (
+                ""
+              )}
+            </th>
+            <th>
+              <button onClick={() => deleteContact(contact.id)}> Delete</button>
+            </th>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
